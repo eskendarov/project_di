b0ab46ebf750df93f4064c1899903d55dc494e3d
Enver Eskendarov <envereskendarov@gmail.com>
2023-12-28 11:35:24 -0800

    Жизненный цикл Spring Bean [#505071]

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("ru.job4j.di");
+        context.close();
+    }
+}
